62db7bb6377ca72c631e0d8fc3ecd3d0c2dc0396
andresbermeoq <andresbermeoq1@gmail.com>
2023-05-15 23:27:46 -0500

    feat: Add random return

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,23 @@
-use std::io;
+use std::{io, println};
 mod methods;
 
 fn main() {
-    println!("Dame un número 1!");
+    println!("Dame un número 1");
 
-    let mut n1 = String::new();
+    let mut n1: String = String::new();
+    io::stdin().read_line(&mut n1).expect("Failed to read line");
 
     println!("Dame un número 2");
 
-    let mut n2 = String::new();
-
-    io::stdin().read_line(&mut n1).expect("Failed to read line");
-
+    let mut n2: String = String::new();
     io::stdin().read_line(&mut n2).expect("Failed to read line");
 
-    let number1: i64 = n1.parse().unwrap();
-    let number2: i64 = n2.parse().unwrap();
+    let number1: i64 = n1.trim().parse().unwrap();
+    let number2: i64 = n2.trim().parse().unwrap();
 
     let res = methods::sumar(number1, number2);
-
-    println!("La suma es:!{:?}", res);
     let guess = methods::get_random();
 
-    println!("El numero es:!{:?}", guess);
+    println!("El valor de la suma: {:?}", res);
+    println!("El numero es: {:?}", guess);
 }
--- src/methods.rs
+++ src/methods.rs
@@ -1,9 +1,8 @@
 use rand::Rng;
-
-pub(crate) fn sumar(number1: i64, number2: i64) {
-    let _ = number1 + number2;
+pub fn sumar(number1: i64, number2: i64) -> i64 {
+    number1 + number2
 }
 
-pub(crate) fn get_random() {
-    rand::thread_rng().gen_range(1..=100);
+pub fn get_random() -> i64 {
+    rand::thread_rng().gen_range(1..=100)
 }
